6bd2c624e3968fe7501c3904eb30eb3469aa3d40
Daniel Bryant <azbryant85@gmail.com>
2019-03-04 10:13:07 -0700

    update final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,16 +9,23 @@ var game = {
     reset: false,
     random: "",
     ranWord: "#",
-    underscore: [],
+    underscore: "",
+    wins: 0,
+    loses: 0,
 
     start: function () {
-        this.underscore = [];
+        this.underscore = "";
         this.lives = 12;
         this.letters = "";
+        document.getElementById("wrong").innerHTML = this.letters;
+        document.getElementById("userGuess").innerHTML = "";
+        document.getElementById("chances").innerHTML = this.lives;
+        // document.getElementById('sound1').play();
+
 
         this.random = this.words[Math.floor(Math.random() * this.words.length)];
 
-        this.ranWord = this.random;
+        this.ranWord = this.random.toUpperCase();
 
         for (var i = 0; i < this.ranWord.length; i++) {
 
@@ -28,7 +35,7 @@ var game = {
             else {
                 this.underscore = this.underscore + "_";
             }
-        }
+        };
 
         document.getElementById("wordfield").innerHTML = this.underscore;
 
@@ -36,12 +43,51 @@ var game = {
         console.log(this.underscore);
         console.log(this.ranWord.length);
         console.log(this.underscore.length);
+
+        if (this.random === "Khaleesi") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/khaleesi.jpeg");
+        }
+        else if (this.random === "Maester") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/maester.jpg");
+        }
+        else if (this.random === "House of Stark") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/hos.jpg");
+        }
+        else if (this.random === "Mother of Dragons") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/mod.jpg");
+        }
+        else if (this.random === "Winter is Coming") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/wic.png");
+        }
+        else if (this.random === "Brotherhood Without Banners") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/bwb.jpg");
+        }
+        else if (this.random === "NIGHTS WATCH") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/nw.png");
+        }
+        else if (this.random === "Kings Landing") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/kl.jpg");
+        }
+        else if (this.random === "The Iron Throne") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/tith.jpg");
+        }
+        else if (this.random === "Warden of the North") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/wotn.jpg");
+        }
+        else if (this.random === "Khal Drogo") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/kd.jpg");
+        }
+        else if (this.random === "The Red Wedding") {
+            document.getElementById('picture').setAttribute("src", "./assets/images/trw.jpg");
+        }
+
     },
 
     logic: function () {
 
-        for (var j = 0; j < this.ranWord.length; j++) {
+        var check = this.underscore;
 
+        for (var j = 0; j < this.ranWord.length; j++) {
 
             if (this.ranWord[j].toLowerCase() === userInput.toLowerCase()) {
 
@@ -52,20 +98,37 @@ var game = {
             }
         }
 
-        if (this.ranWord.toLowerCase() === userInput.toLowerCase()) {
-            ;
-        }
-        else {
-            this.letters += userInput.toUpperCase();
+        if (check === this.underscore) {
+            if (this.letters.charAt(0) === userInput.toUpperCase() || this.letters.charAt(1) === userInput.toUpperCase() ||
+                this.letters.charAt(2) === userInput.toUpperCase() || this.letters.charAt(3) === userInput.toUpperCase() ||
+                this.letters.charAt(4) === userInput.toUpperCase() || this.letters.charAt(5) === userInput.toUpperCase() ||
+                this.letters.charAt(6) === userInput.toUpperCase() || this.letters.charAt(7) === userInput.toUpperCase() ||
+                this.letters.charAt(8) === userInput.toUpperCase() || this.letters.charAt(9) === userInput.toUpperCase() ||
+                this.letters.charAt(10) === userInput.toUpperCase() || this.letters.charAt(11) === userInput.toUpperCase()) {
+
+            }
+            else {
+                this.letters += userInput.toUpperCase();
+                document.getElementById("wrong").innerHTML = this.letters;
+                --this.lives;
+                document.getElementById("chances").innerHTML = this.lives;
+            }
         }
 
-        document.getElementById("wrong").innerHTML = this.letters;
         document.getElementById("wordfield").innerHTML = this.underscore;
 
         if (this.underscore === this.ranWord) {
+            document.getElementById('sound1').pause();
             setTimeout(function () {
                 game.win()
-            }, 1000)
+            }, 500)
+        }
+
+        if (this.lives === 0) {
+            document.getElementById('sound1').pause();
+            setTimeout(function () {
+                game.lose()
+            }, 500)
         }
     },
 
@@ -74,7 +137,19 @@ var game = {
     },
 
     win: function () {
+        document.getElementById('winsound').play();
         alert("You Win!");
+        ++this.wins
+        document.getElementById("wins").innerHTML = this.wins;
+        game.start();
+    },
+
+    lose: function () {
+        document.getElementById('losesound').play();
+        alert("You Lose!");
+        ++this.loses;
+        document.getElementById("loses").innerHTML = this.loses;
+        game.start();
     },
 };
 
@@ -85,5 +160,5 @@ document.onkeyup = function (event) {
     game.logic();
 
     console.log(userInput);
-    document.getElementById("userGuess").innerHTML = userInput;
+    document.getElementById("userGuess").innerHTML = userInput.toUpperCase();
 }
